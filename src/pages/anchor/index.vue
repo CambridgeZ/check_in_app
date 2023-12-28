815d8dd04417d4a0b59cbabc0c658142306952d1
CambridgeZ <1376734195@qq.com>
2023-12-28 20:28:22 +0800

    start

---
+++ src/pages/anchor/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="startup-page">
+        <h1 style="color: navy;">Welcome!</h1>
+        <button @click="goToLoginPage">登录</button> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StartupPage",
+    methods: {
+        goToLoginPage() {
+            uni.navigateTo({
+                url: '/pages/login' 
+            });
+        }
+    },
+};
+</script>
+
+<style scoped>
+.startup-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 36px;
+    font-weight: bolder;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    background-color: antiquewhite;
+}
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input type="text" v-model="username" placeholder="Username" required>
+            <input type="password" v-model="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        async login() {
+            const encoder = new TextEncoder();
+            const data = encoder.encode(this.password);
+            const hash = await window.crypto.subtle.digest('SHA-256', data);
+            const hashedPassword = Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
+
+            fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: this.username,
+                    password: hashedPassword
+                })
+            }).then(res => res.json())
+                .then(res => {
+                    if (res.success) {
+                        uni.setStorageSync('token', res.token);
+                        uni.setStorageSync('username', this.username);
+                        uni.switchTab({
+                            url: '/pages/index/index'
+                        });
+                    } else {
+                        uni.showToast({
+                            title: res.message,
+                            icon: 'none'
+                        });
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                    uni.showToast({
+                        title: 'Login failed',
+                        icon: 'none'
+                    });
+                });
+        }
+    }
+};
+</script>
+
+<style scoped>
+.login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+h1 {
+    margin-bottom: 2rem;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+input {
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    width: 15rem;
+}
+
+button {
+    padding: 0.5rem 1rem;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+</style>
